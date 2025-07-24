fc933a0f373d6d14750a5b69572c73444442200b
iniidara <iniidaraisaiah@gmail.com>
2025-07-24 14:54:46 +0100

    fifth

--- display.js
+++ display.js
@@ -1,4 +1,4 @@
-function showSection() {
+    function showSection() {
       document.getElementById('section1').style.display = 'none';
       document.getElementById('section2').style.display = 'grid';
       document.getElementById('section3').style.display = 'none';
@@ -8,7 +8,7 @@ function showSection() {
       document.getElementById('section1').style.display = 'grid';
       document.getElementById('section2').style.display = 'none';
       document.getElementById('section3').style.display = 'none';
-      document.getElementById('form').style.display = 'none'
+      document.getElementById('form').style.display = 'none';
     }
     function showAll() {
       document.getElementById('section1').style.display = 'grid';
@@ -20,6 +20,8 @@ function showSection() {
       document.getElementById('section2').style.display = 'none';
       document.getElementById('section3').style.display = 'grid';
     }
-    function showLogin() {
-        document.getElementById('form').style.display = 'flex'
-    }
+function showLogin() {
+    document.getElementById('form').style.display = 'flex';
+}
+
+    
