4deefae7c2f4271d858a187577168913a501a35f
iniidara <iniidaraisaiah@gmail.com>
2025-07-24 12:48:38 +0100

    login form

--- display.js
+++ display.js
@@ -8,6 +8,7 @@ function showSection() {
       document.getElementById('section1').style.display = 'grid';
       document.getElementById('section2').style.display = 'none';
       document.getElementById('section3').style.display = 'none';
+      document.getElementById('form').style.display = 'none'
     }
     function showAll() {
       document.getElementById('section1').style.display = 'grid';
@@ -18,4 +19,7 @@ function showSection() {
       document.getElementById('section1').style.display = 'none';
       document.getElementById('section2').style.display = 'none';
       document.getElementById('section3').style.display = 'grid';
+    }
+    function showLogin() {
+        document.getElementById('form').style.display = 'flex'
     }
