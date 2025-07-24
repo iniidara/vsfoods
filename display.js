d78a3a52b682ac3ec1d74398bed714e7d2e226c2
iniidara <iniidaraisaiah@gmail.com>
2025-07-24 10:36:28 +0100

    display

---
+++ display.js
@@ -0,0 +1,21 @@
+function showSection() {
+      document.getElementById('section1').style.display = 'none';
+      document.getElementById('section2').style.display = 'grid';
+      document.getElementById('section3').style.display = 'none';
+    }
+    // Show both sections by default
+    window.onload = function() {
+      document.getElementById('section1').style.display = 'grid';
+      document.getElementById('section2').style.display = 'none';
+      document.getElementById('section3').style.display = 'none';
+    }
+    function showAll() {
+      document.getElementById('section1').style.display = 'grid';
+      document.getElementById('section2').style.display = 'none';
+      document.getElementById('section3').style.display = 'none';
+    }
+    function showPizza() {
+      document.getElementById('section1').style.display = 'none';
+      document.getElementById('section2').style.display = 'none';
+      document.getElementById('section3').style.display = 'grid';
+    }
